6293fa0009d8ee3062a1abc58e478046a6f70453
widmogrod <widmogrod@gmail.com>
2024-07-03 17:02:02 +0200

    update WPS=1

--- dev/run.sh
+++ dev/run.sh
@@ -14,7 +14,7 @@ echo "Wait for 15s for compose containers to start..."
 sleep 15
 
 echo "Benchmarking Elastic"
-ELASTIC_AUTH="" ELASTIC_URL=http://localhost:9200 ELASTIC_INDEX=index_name WPS=2 BATCH_SIZE=50 DESTINATION=Elastic TRACK_LATENCY=true ./rockbench
+ELASTIC_AUTH="" ELASTIC_URL=http://localhost:9200 ELASTIC_INDEX=index_name WPS=1 BATCH_SIZE=50 DESTINATION=Elastic TRACK_LATENCY=true ./rockbench
 
 Echo "Benchmarking CrateDB"
-CRATEDB_URI="postgres://crate:@localhost:5432/test?pool_max_conns=10&pool_min_conns=3" WPS=2 BATCH_SIZE=50 DESTINATION=CrateDB TRACK_LATENCY=true ./rockbench
+CRATEDB_URI="postgres://crate:@localhost:5432/test?pool_max_conns=10&pool_min_conns=3" WPS=1 BATCH_SIZE=50 DESTINATION=CrateDB TRACK_LATENCY=true ./rockbench
