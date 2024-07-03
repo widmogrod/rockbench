f72c1e2c6f98aaf0f4332b3f270b94da681b81b2
widmogrod <widmogrod@gmail.com>
2024-07-03 16:22:10 +0200

    dev/run.sh allows simple run of benchmarks

--- dev/run.sh
+++ dev/run.sh
@@ -6,8 +6,11 @@ project_root=$(dirname "$cwd")
 docker compose -f $cwd/compose.yml up -d
 trap "docker compose -f $cwd/compose.yml down" EXIT
 
+echo "Build rockbench"
+go build .
+
 echo "Benchmarking Elastic"
 ELASTIC_AUTH="" ELASTIC_URL=http://localhost:9200 ELASTIC_INDEX=index_name WPS=1 BATCH_SIZE=50 DESTINATION=Elastic TRACK_LATENCY=true ./rockbench
 
 Echo "Benchmarking CrateDB"
-CRATEDB_URI="postgres://crate:@localhost:5432/test" WPS=1 BATCH_SIZE=50 DESTINATION=CrateDB TRACK_LATENCY=true ./rockbench
+CRATEDB_URI="postgres://crate:@localhost:5432/test?pool_max_conns=10&pool_min_conns=3" WPS=1 BATCH_SIZE=50 DESTINATION=CrateDB TRACK_LATENCY=true ./rockbench
